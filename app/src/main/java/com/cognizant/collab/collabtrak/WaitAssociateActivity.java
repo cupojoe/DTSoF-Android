765a0c256e24733ef80b62435f97ff2bc1205a57
Joel Fraga <joe.fraga@gmail.com>
2017-01-17 10:08:34 -0500

    Inverted colors and added a new view

--- app/src/main/java/com/cognizant/collab/collabtrak/WaitAssociateActivity.java
+++ app/src/main/java/com/cognizant/collab/collabtrak/WaitAssociateActivity.java
@@ -1,10 +1,8 @@
 package com.cognizant.collab.collabtrak;
 
 
-import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
@@ -12,24 +10,15 @@ import android.widget.TextView;
  * Created by ctsuser1 on 1/16/17.
  */
 
-public class ReadyNotificationActivity extends AppCompatActivity {
+public class WaitAssociateActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.ready_notification);
+        setContentView(R.layout.wait_associate);
 
         TextView tx = (TextView)findViewById(R.id.spinner);
         Typeface iconFont = Typeface.createFromAsset(getAssets(), "fonts/TeleIcon-Outline.ttf");
         tx.setTypeface(iconFont);
-
-        final Handler delayedActivity = new Handler();
-        delayedActivity.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent myIntent = new Intent(getBaseContext(), WaitAssociateActivity.class);
-                startActivity(myIntent);
-            }
-        }, 3000);
     }
 }
